33d6c0e35ae42da4a0c2c7227868d4efdce077ae
Natalia504 <natalia.calt@gmail.com>
2018-04-01 10:52:36 -0600

    confid file created

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -4,39 +4,46 @@ import { Link } from 'react-router-dom';
 import { itemDetails } from '../reducer';
 import parseDate from './../utils';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import apiKey from '../config';
 
 class ItemDetails extends Component {
 
     componentDidMount() {
         this.props.itemDetails(this.props.match.params.id);
     }
+    onMarkerClick(){
+        return this.props.item.map(e => e.location.name)
+    }
 
     render() {
         console.log(this.props.item, this.props.lat, this.props.lng)
         const oneItem = this.props.item.map((e, i) => {
             return <div key={e.id} className=''>
-                <div className='row'>Address: {e.location.name}</div>
                 <div className='row'>Name: {e.name}</div>
+                <div className='row'>Location: {e.location.name}</div>
+                <div className='row'>Size: {e.size}</div>
                 <div className='row'>ID: {e.id}</div>
                 <div className='row'>Serial: {e.serial}</div>
-                <div className='row'>Size: {e.size}</div>
                 <div className='row'>Date Created: {parseDate(e.createdDate)}</div>
                 <div className='row'>Modified On: {parseDate(e.modifiedDate)}</div>
             </div>
-
         })
+        const address = this.props.item.map((e, i) => {
+            return <div key={e.id} className=''>
+                <div className='row'>Address: {e.location.name}</div>
+                </div>
+                })
         return (
             <div className='found'>
                 <Link to="/"><button>Back</button></Link>
 
                 <div className='data'>{oneItem}</div>
 
-                <div>
+                <div className='map'>
                     {this.props.lat && this.props.lng ?
                         <Map
                             style={{
-                                width: '50%', height: '50%', top: '70%',
-                                left: '25%', border: '3px solid red', position: 'relative'
+                                width: '80%', height: '80%', margin: 'auto', border: '1px solid grey', position: 'relative'
                             }}
                             google={this.props.google}
                             zoom={16}
@@ -47,7 +54,7 @@ class ItemDetails extends Component {
                         >
                             <Marker
                                 onClick={this.onMarkerClick}
-                                name={'Current location'} />
+                                name={address} />
                             <InfoWindow>
                                 <div>
                                     <h1>Selected Place</h1>
@@ -70,10 +77,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, { itemDetails })(GoogleApiWrapper({ apiKey: 'AIzaSyDjl5AJyP6VeXtPMjScwjAQORt0rvkOyBE' })(ItemDetails));
-
-// AIzaSyDjl5AJyP6VeXtPMjScwjAQORt0rvkOyBE
-//https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap
-// export default GoogleApiWrapper({
-//   apiKey: (YOUR_GOOGLE_API_KEY_GOES_HERE)
-// })(MapContainer)
+export default connect(mapStateToProps, { itemDetails })(GoogleApiWrapper({ apiKey: apiKey.key })(ItemDetails));
