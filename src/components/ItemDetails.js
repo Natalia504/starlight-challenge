9036fb292a792c0aebeefa3560d98392967f6941
Natalia504 <natalia.calt@gmail.com>
2018-04-02 09:33:30 -0600

    maps re-renders properly

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -5,11 +5,12 @@ import { itemDetails, setCoordinates } from '../reducer';
 import parseDate from './../utils';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import apiKey from '../config';
-import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 
 // have to use two libraries: google-maps-react & react-places-autocomplete;
 // the original data have inaccurate coordinates;
 // so, have to look up the correct coordinates by the address with react-places-autocomplete and then feed it to the google-maps-react to get a map. 
+// some addresses are inaccurate! Ex:2200 W. Famous Rd, Aurora, CO 80018(no such road);
 
 
 class ItemDetails extends Component {
@@ -17,26 +18,13 @@ class ItemDetails extends Component {
     componentDidMount() {
         this.props.itemDetails(this.props.match.params.id)
         .then(() => geocodeByAddress(this.props.address)
-            .then(results => {
-                console.log('1', results)
-                return getLatLng(results[0])
-            })
-            .then(results => { console.log('2', results)
-                this.props.setCoordinates(results)
-            })
+            .then(results => getLatLng(results[0]))
+            .then(results => this.props.setCoordinates(results))
             .catch(error => console.error('Error', error))
         )
     }
 
-
     render() {
-        console.log(this.props.item, this.props.lat, this.props.lng)
-        console.log(this.props.latLng, 'LatLNG')
-        const inputProps = {
-            value: this.props.address,
-            onChange: this.onChange
-        }
-        console.log(inputProps, 'props')
 
         const oneItem = this.props.item.map((e, i) => {
             return <div key={e.id} className=''>
@@ -49,10 +37,10 @@ class ItemDetails extends Component {
                 <div className='row'>Modified On: {parseDate(e.modifiedDate)}</div>
             </div>
         })
-       
+
         return (
             <div className='found'>
-                <Link to="/"><button>Back</button></Link>
+                <Link to="/"><button onClick={() => this.props.setCoordinates({})}>Back</button></Link>
 
                 <div className='data'>{oneItem}</div>
 
@@ -60,8 +48,8 @@ class ItemDetails extends Component {
                     {Object.keys(this.props.latLng).length !== 0 ?
                         <Map
                             style={{
-                                width: '80%',
-                                height: '80%',
+                                width: '60%',
+                                height: '50%',
                                 margin: 'auto',
                                 border: '1px solid grey',
                                 position: 'relative'
@@ -90,8 +78,6 @@ class ItemDetails extends Component {
 const mapStateToProps = state => {
     return {
         item: state.item,
-        lat: state.lat,
-        lng: state.lng,
         latLng: state.latLng,
         address: state.address
     }
